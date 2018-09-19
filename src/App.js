e49745645afb0f82d44ae2463d3e2b7fba30850c
UDAY BAG <uday625@Nemesis-2.local>
2018-09-18 23:42:11 -0400

    first version

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Welcome to Movie Mart- React app</p>
+      </div>
+    );
+  }
+}
+
+export default App;
